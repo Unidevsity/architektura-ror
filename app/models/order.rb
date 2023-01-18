652f60b16dc208ad81c26b041645b214db8689e0
Rafal Piekara <rafal@piekara.me>
2023-01-18 11:53:51 +0100

    Add relations between order, product and order lines

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,5 +8,5 @@
 #  updated_at :datetime         not null
 #
 class Order < ApplicationRecord
-
+  has_many :order_lines, dependent: :destroy
 end
--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -17,4 +17,6 @@
 #  index_order_lines_on_product_id  (product_id)
 #
 class OrderLine < ApplicationRecord
+  belongs_to :order, inverse_of: :order_lines
+  belongs_to :product, inverse_of: :order_lines
 end
