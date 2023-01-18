f5cdbfd8b53621e8d1e17e80a418b78a830976bf
Rafal Piekara <rafal@piekara.me>
2023-01-18 16:59:08 +0100

    Extreact update order lines to separate method

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,16 @@ class Order < ApplicationRecord
   has_many :order_lines, dependent: :destroy, inverse_of: :order, autosave: true
 
   accepts_nested_attributes_for :order_lines, reject_if: :all_blank, allow_destroy: true
+
+  def update_lines(order_lines_attributes)
+    order_lines_attributes.each do |order_line|
+      if order_lines.find_by(product_id: order_line[:product_id])
+        order_line[:quantity] = order_line[:quantity].to_i + order_lines.find_by(product_id: order_line[:product_id]).quantity
+        order_lines.find_by(product_id: order_line[:product_id]).build(quantity: order_line[:quantity])
+      else
+        order_lines.build(order_line)
+      end
+    end
+    save!
+  end
 end
